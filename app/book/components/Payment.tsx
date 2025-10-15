5de83c40f4fc2aa3c7308d1d046ad34550869dd6
Ashish Singh Negi <ashishamonegi@gmail.com>
2025-10-15 11:06:41 +0530

    fix payment ui

--- app/book/components/Payment.tsx
+++ app/book/components/Payment.tsx
@@ -6,9 +6,9 @@ const Payment = () => {
   return (
     <section className="w-full flex flex-col lg:flex-row justify-center gap-6 lg:gap-10 mt-6 sm:px-4">
       <PaymentSelection />
-      <div className="w-fit flex flex-col items-center">
+      <div className="flex flex-col">
         <PriceSummary />
-        <div className="w-full mt-2 flex gap-2">
+        <div className="w-full sm:w-[348px] mt-2 flex justify-between gap-2">
           <p className="w-28 text-[10px] sm:text-sm font-semibold flex flex-col items-center text-center gap-2">
             <Image
               src={"/icons/Sheild-icon.svg"}
